227ee5c8dce253e0b0cfdd38544ec4e16da5f8ce
jro <jari.rosti@gmail.com>
2011-06-28 18:46:20 +0300

    .

---
+++ src/main/java/Continuation.java
@@ -0,0 +1,79 @@
+import java.util.Stack;
+
+public class Continuation {
+
+	interface Function<T, K> {
+		T apply(K v);
+	}
+
+	interface BinOp<T> {
+		T apply(T a, T b);
+	}
+	
+	static Function<Function<Integer, Integer>, Integer> op(final BinOp<Integer> op) {
+		
+		return new Function<Function<Integer, Integer>, Integer>() {
+			
+			@Override
+			public Function<Integer, Integer> apply(final Integer left) {
+				return new Function<Integer, Integer>() {
+					
+					@Override
+					public Integer apply(Integer right) {
+						return op.apply(left, right);
+					}
+				};
+			}
+		};
+	}
+	
+	static Function<Function<Integer, Integer>, Integer> times() {
+		return op(new BinOp<Integer>() {
+			
+			@Override
+			public Integer apply(Integer a, Integer b) {
+				return a*b;
+			}
+		});
+	}
+
+	static <T> Function<T, T> id(T a) {
+		return new Function<T, T>() {
+			@Override
+			public T apply(T v) {
+				return v;
+			}
+		}; 
+	}
+	
+	public static Integer faccps(Function<Integer, Integer> continuation, Integer n) {
+		if (n == 0) return continuation.apply(1);
+		return faccps(times().apply(continuation.apply(n)), n-1);
+	}
+	
+	public static Integer fac(Integer a) {
+		return faccps(id(a) , a);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(curry(times(), 1, 2, 3, 4, 5));
+		// 
+		System.out.println(fac(5));
+	}
+
+	public static Integer curry(Function<Function<Integer, Integer>, Integer> op, Integer ... args) {
+		Stack<Integer> argList = new Stack<Integer>();
+		for  (Integer arg : args) {
+			argList.push(arg);
+		}
+		Integer res = argList.pop();
+		do {
+			Function<Integer, Integer> f = op.apply(res);
+			res = f.apply(argList.pop());
+		} while(! argList.isEmpty());
+		return res; 
+	}
+}
+
+
+

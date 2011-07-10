82a9d37ef7e9c12570b1d63ee935d7dd4f4a7a7a
jro <jari.rosti@gmail.com>
2011-07-10 13:50:57 +0300

    some test changes

--- javafp-and-scala/src/main/scala/Factorials.scala
+++ javafp-and-scala/src/main/scala/Factorials.scala
@@ -55,42 +55,35 @@ object Factorials {
 	def fac8(n: Int) = (1 to n) product
 	
 	type Impls = List[Pair[Int=>Int, String]]	
-	val impls : Impls = List(  Pair(fac0, "Iterative while")
-						, Pair(fac01, "Iterative for-loop through a sequence")
-						, Pair(fac1, "Iterative foreach sequence with a side effect lambda")
-						, Pair(fac2, "Classic recursive")
-						, Pair(fac3, "Reducing from left")
-						, Pair(fac4, "Folding from left")
-						, Pair(fac5, "Infinite secuence generator")
-						, Pair(fac6, "Accumulated parameter recursion")
-						, Pair(fac7, "Continuation passing style")
-						, Pair(fac8, "Product operation to a sequence.")
-						)
+	val impls : Impls = List(  Pair(fac0, "Iterative while                  ")
+				 , Pair(fac01,"Iterative for through a sequence ")
+				 , Pair(fac1, "Iterative foreach with lambda    ")
+				 , Pair(fac2, "Classic recursive                ")
+				 , Pair(fac3, "Reducing from left               ")
+				 , Pair(fac4, "Folding from left                ")
+				 , Pair(fac5, "Infinite secuence generator      ")
+				 , Pair(fac6, "Accumulated parameter recursion  ")
+				 , Pair(fac7, "Continuation passing style       ")
+				 , Pair(fac8, "Product operation to a sequence  ")
+				 )
 
-	val computeDefault = compute(29, 10000, _ :Int=>Int, _ :String) 
+	val computeDefault = compute(100000, 100, _ :Int=>Int, _ :String) 
 
-	def compute(n: Int, iterations: Int, funct :Int=>Int, name : String) : Int = {
-		println("Implementation: " + name)
-		println("Computing " + n + "! = " + funct(n) + " repeat " + iterations + " times.")
+	def compute(n: Int, iterations: Int, funct :Int=>Int, name : String) :Unit = {
 		var i :Int = 0 
 		val start = System.currentTimeMillis()
 		while (i < iterations) {
-			funct(n)
-			i += 1
+		      funct(n)		      
+		      i += 1
 		}
 		val stop = System.currentTimeMillis()
 		stop - start
+		println(name + " " + (stop - start) + " ms")
 	}
-}
-
-
-object Main {
 	
   def main(args : Array[String]) : Unit = {
-	 import Factorials._
 	 val run =  (_:Unit) => {for (impl <- impls) {
-	   val millis = computeDefault(impl _1, impl _2)
-	   println(millis + "ms\n")
+	   computeDefault(impl _1, impl _2)
 	 }
 	}
 	run(1)

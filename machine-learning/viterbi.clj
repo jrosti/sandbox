6022cee3b5523b10dfc7a24955aca2877658bae0
jro <jari.rosti@gmail.com>
2014-01-30 00:29:00 +0200

    viterbi inner loop written in clojure for comparing JS version

---
+++ machine-learning/viterbi.clj
@@ -0,0 +1,24 @@
+(def transitions 
+  [[0.3 0.5 0.8] 
+   [0.1 0.8 0.1] 
+   [0.1 0.7 0.2]])
+
+(defn observation-fn
+  [state token]
+  0.3333)
+
+(def states 3)
+
+(defn viterbi [accumulator observation]
+  (loop [next 0 newViterbi [] newPath []]
+    (if (= source states) 
+      {:path newPath :viterbi newViterbi}
+      (let [candidates (map-indexed (fn [source]
+                                        (* ((:viterbi accumulator) source) 
+                                           (get-in transitions [source next])
+                                           (observation-fn observation next)))
+                                    (range states))
+            best (apply min-key second candidates)]
+        (recur (inc source) 
+               (conj newViterbi (second best))
+               (conj ((:path accumulator) (first best)) next))))))

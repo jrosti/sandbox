6b6ceda30aeac8b98028a6a22572a42dc33e7103
jro <jari.rosti@gmail.com>
2014-01-30 23:20:15 +0200

    transients and logarithmic sums for probabilities

--- machine-learning/viterbi.clj
+++ machine-learning/viterbi.clj
@@ -9,19 +9,24 @@
 
 (def states 3)
 
-(defn viterbi 
-  [accumulator observation]
-  (loop [next 0 new-viterbi [] new-path []]
-    (if (= source states) 
-      {:path new-path :viterbi new-viterbi}
-      (let [candidates (map-indexed (fn [source]
-                                        (* ((:viterbi accumulator) source) 
-                                           (get-in transitions [source next])
-                                           (observation-fn observation next)))
-                                    (range states))
-            survivor (apply min-key second candidates)]
-        (recur (inc source) 
-               (conj new-viterbi (second survivor))
-               (conj new-path 
-                     (conj ((:path accumulator) (first survivor)) next)))))))
+(def initial-state 
+  [ [ [0] [1] [2]] [-1 -1 -1] ])
 
+(defn apply-viterbi 
+  [[path viterbi] observation]
+  (loop [j 0 new-viterbi (transient []) new-path (transient [])]
+    (if (= j states)
+      [(persistent! new-path) (persistent! new-viterbi)]
+      (let [candidates (for [i (range states)]
+                         [i (+ (viterbi i) 
+                               (Math/log (get-in transitions [i j]))
+                               (Math/log (observation-fn observation j)))])
+            survivor (apply max-key second candidates)]
+        (recur (inc j) 
+               (conj! new-viterbi (second survivor))
+               (conj! new-path 
+                     (conj (path (first survivor)) j)))))))
+
+(defn run []
+  (reduce apply-viterbi initial-state 
+          (take 100 (interpose 2 (repeat 0)))))

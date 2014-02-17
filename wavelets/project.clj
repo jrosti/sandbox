be1e3adcbf8ba38f5b5bc0e56e4f3cca9b04d18e
jro <jari.rosti@gmail.com>
2014-02-17 16:10:58 +0200

    project description

--- wavelets/project.clj
+++ wavelets/project.clj
@@ -1,5 +1,5 @@
 (defproject jwave "0.0.1"
-  :description "A training blog."
+  :description "JWave Clojure toy for studying wavelets."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [junit/junit-dep "4.11"]]
   :main jwave.core

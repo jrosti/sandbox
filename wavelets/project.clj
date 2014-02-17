677edfc57b00d1f34dc41342f970bde091c58417
jro <jari.rosti@gmail.com>
2014-02-17 09:54:06 +0200

    JWave and clojure repl toy

---
+++ wavelets/project.clj
@@ -0,0 +1,7 @@
+(defproject jwave "0.0.1"
+  :description "A training blog."
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [junit/junit-dep "4.11"]]
+  :main jwave.core
+  :java-source-paths ["src/main/java" "src/test/java"])
+
---
+++ wavelets/src/jwave/core.clj
@@ -0,0 +1,124 @@
+(ns jwave.core
+  [:import 
+   [math.jwave.transforms.wavelets 
+    Haar02 Coif06 Daub02 Daub03 Daub04 Haar02Orthogonal Lege02 Lege04 Lege06]
+   [math.jwave Transform]
+   [math.jwave.transforms FastWaveletTransform WaveletPacketTransform]
+   [javax.imageio ImageIO]
+   [java.io File]
+   [java.awt.image BufferedImage DataBufferByte]
+   [java.awt Color Dimension Graphics]
+   [javax.swing JFrame JPanel]
+   ])
+
+(defn ftw [transform] 
+  (Transform. (FastWaveletTransform. transform)))
+
+(defn wpt [transform]
+  (Transform. (WaveletPacketTransform. transform)))
+
+(def haar-ftw
+  (ftw (Haar02.)))
+
+(def haar-wpt
+  (wpt (Haar02.)))
+
+(defn to-2d-double 
+  [coll]
+  (into-array (map double-array coll)))
+
+(defn forward 
+  [ftw double-arr]
+  (.forward ftw double-arr))
+
+(defn forward-wpt
+  [wpt double-arr]
+  (.forward wpt double-arr))
+
+(def haar-forward
+  (partial forward haar-ftw))
+
+(def haar-wpt-forward 
+  (partial forward-wpt haar-wpt))
+
+(defn read-buffered-image 
+  [filename]
+  (ImageIO/read (File. filename)))
+
+(defn rgb-to-double [rgb]
+  (let [r (bit-and (bit-shift-right rgb 16) 255)
+        g (bit-and (bit-shift-right rgb 8) 255)
+        b (bit-and rgb 255)]
+    (mapv (fn [v] (/ (double v) 255.0)) [r g b])))
+
+(defn intensity
+  [buffered-image]
+  (let [h (.getHeight buffered-image)
+        w (.getWidth buffered-image)
+        mk #(make-array Double/TYPE w h)
+        arr-r (mk) arr-g (mk) arr-b (mk)]
+    (dotimes [y h]
+      (dotimes [x w]
+        (let [[r g b] (rgb-to-double (.getRGB buffered-image x y))]
+          (aset arr-r x y r)
+          (aset arr-g x y g)
+          (aset arr-b x y b))))
+    [arr-r arr-g arr-b]))
+
+(defn construct-frame [width height panel]
+  (let [frame (JFrame.)]
+      (.setPreferredSize panel (Dimension. width height))
+      (doto frame
+        (.add panel)
+        .pack
+        (.setLocationRelativeTo nil)
+        .show)))
+
+(defn create-image [width height]
+  (BufferedImage. width height (BufferedImage/TYPE_INT_RGB)))
+
+(defn get-panel [img]
+  (proxy [JPanel] [] (paint [g] (.drawImage g img 0 0 (Color/red) nil))))
+
+(defn amaxmin [arr w h]
+  (let [maxmin (double-array [Integer/MIN_VALUE Integer/MAX_VALUE 0.0])]
+    (dotimes [y h]
+      (dotimes [x w]
+        (if (> (aget arr x y) (aget maxmin 0)) (aset maxmin 0 (aget arr x y))) 
+        (if (< (aget arr x y) (aget maxmin 1)) (aset maxmin 1 (aget arr x y)))
+        (aset maxmin 2 (+ (aget maxmin 2) (aget arr x y)))))
+    (aset maxmin 2 (/ (aget maxmin 2) (* w h)))
+    (vec maxmin)))
+
+(defn draw-result [output-panel wr w h transform]
+  (let [[max min mean] (amaxmin transform w h)
+        mm (double (- min))
+        rr (double (+ max mm))]
+    (prn "transform max min" max min mm rr mean)
+    (dotimes [y h]
+      (dotimes [x w]
+        (let [val (double (aget transform x y))
+              g (int (if (< val -0.01) 255 0))
+              r (if (> val 0.005) 255 0)
+              b (if (and (< val 0.005) (> val -0.01)) 255 0)]
+          (.setPixel wr x y (int-array [r g b]))))
+        (.repaint output-panel))))
+  
+(defn -main [& args]
+  (let [input-image (future (read-buffered-image (first args)))
+        width (future (.getWidth @input-image))
+        height (future (.getHeight @input-image))
+        output-images (future (map (fn [x] (create-image @width @height)) (range 3)))
+        output-panels (map (partial get-panel) @output-images)
+        input-panel (get-panel @input-image)
+        rasters (map (fn [img] (.getRaster img)) @output-images)]
+    (pmap #(construct-frame @width @height %) output-panels)
+    (construct-frame @width @height input-panel)
+    (let [trs (intensity @input-image)
+          waves (pmap haar-forward trs)
+          draw (fn [f t] (draw-result 
+                          (f output-panels) 
+                          (f rasters) @width @height t))]
+      (doall (pmap draw [first second last] waves))          
+      (prn "done"))))
+
